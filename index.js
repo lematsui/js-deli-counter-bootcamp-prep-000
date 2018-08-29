2c17687c7d1965b66d42e87f42fb7b1397b45d89
Ariel Matsui-LeRoy <arielmatsuileroy@gmail.com>
2018-08-29 17:57:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(theLine) {
     for (var i = 0; theLine.length >= i; i++) {
       thePeople = thePeople + (i + 1) + ". " + theLine[i] + ", ";
     }
-    
+    thePeople = thePeople.slice(0, theLine.length - 2);
     return "The line is currently: " + thePeople;
     }
    else {
