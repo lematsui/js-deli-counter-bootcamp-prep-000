68c5c4eec2b1ec0dd3b22038a571d5127d773142
Ariel Matsui-LeRoy <arielmatsuileroy@gmail.com>
2018-08-29 16:30:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+  else {
+    return "There is nobody waiting to be served!"; 
+  }
+}
+
+function currentLine(theLine) {
+  var thePeople = [];
+  if (theLine.length > 0) {
+    for (var i = 0; i 4)
+  }
+  
+  else {
+    return "The line is currently empty."
+  }
+}
+    
+
+
+    
+  
+  
+
+
+
+  
