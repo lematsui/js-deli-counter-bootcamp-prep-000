6e29f43e058c300c51785153cf433fa10a243520
Ariel Matsui-LeRoy <arielmatsuileroy@gmail.com>
2018-08-29 18:47:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(theLine) {
     for (var i = 0; theLine.length > i + 1; i++) {
       thePeople = thePeople + (i + 1) + ". " + theLine[i] + ", ";
     }
-    for (var o = theLine.length; theLine.length = o; o++) {
+    for (var o = theLine.length; theLine.length === o; o++) {
       thePeople = thePeople + (o + 1) + ". " + theLine[o];
     }
     var message = "The line is currently: " + thePeople;
